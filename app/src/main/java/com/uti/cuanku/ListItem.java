056fe0a1d83b3857b30ba626dd8785930e8f81d0
akbarhlubis <alubis87@gmail.com>
2022-01-21 12:17:29 +0700

    update list item name

--- app/src/main/java/com/uti/cuanku/ListItem.java
+++ app/src/main/java/com/uti/cuanku/ListItem.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class list_item extends AppCompatActivity {
+public class ListItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
