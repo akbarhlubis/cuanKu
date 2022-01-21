e9e11bb750ca6d197cb32635fd10d3c1ce21d916
akbarhlubis <alubis87@gmail.com>
2022-01-21 12:05:34 +0700

    update

--- app/src/main/java/com/uti/cuanku/SplashScreen.java
+++ app/src/main/java/com/uti/cuanku/SplashScreen.java
@@ -2,6 +2,7 @@ package com.uti.cuanku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
@@ -10,5 +11,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                }
+            }
+        };
+        thread.start();
     }
 }
