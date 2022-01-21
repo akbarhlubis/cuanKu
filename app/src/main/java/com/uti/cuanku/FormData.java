046c03ca9915ea9c7d2bddf9a47ce1ad0f38f9e4
akbarhlubis <alubis87@gmail.com>
2022-01-21 10:17:42 +0700

    first commit

---
+++ app/src/main/java/com/uti/cuanku/FormData.java
@@ -0,0 +1,13 @@
+package com.uti.cuanku;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_data);
+    }
+}
