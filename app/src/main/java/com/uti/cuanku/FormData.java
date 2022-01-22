ad73d631706b474a8920ec367f6914413bc6067a
akbarhlubis <alubis87@gmail.com>
2022-01-22 09:08:33 +0700

    membuat adapter untuk fragment

--- app/src/main/java/com/uti/cuanku/FormData.java
+++ app/src/main/java/com/uti/cuanku/FormData.java
@@ -1,13 +1,33 @@
 package com.uti.cuanku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class FormData extends AppCompatActivity {
+
+    //deklarasi dari variabel layout dan viewpager
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_data);
+
+        //Membuat definisi tab layout dengan fragment
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new PemasukanFragment(), "Pemasukan");
+        vpAdapter.addFragment(new PengeluaranFragment(), "Pengeluaran");
+        viewPager.setAdapter(vpAdapter);
     }
 }
