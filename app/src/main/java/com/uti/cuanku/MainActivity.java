87130be251505fdbd2bd879486174b1055099265
akbarhlubis <alubis87@gmail.com>
2022-01-22 20:56:49 +0700

    membuat intent explicit

--- app/src/main/java/com/uti/cuanku/MainActivity.java
+++ app/src/main/java/com/uti/cuanku/MainActivity.java
@@ -2,9 +2,13 @@ package com.uti.cuanku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton add_cuan = findViewById(R.id.add_cuan);
+
+        add_cuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:ini intent yang sifatnya temporary alias sementara, buat uji coba aja. note by akbar
+                startActivity(new Intent(MainActivity.this, FormData.class));
+            }
+        });
     }
 
     @Override
