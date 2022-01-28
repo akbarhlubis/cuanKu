89b0e2ac714b7eb1d8535e536b683529ed90d4fc
akbarhlubis <alubis87@gmail.com>
2022-01-28 07:55:02 +0700

    Membuat Intro Slider #3

--- app/src/main/java/com/uti/cuanku/SplashScreen.java
+++ app/src/main/java/com/uti/cuanku/SplashScreen.java
@@ -19,7 +19,7 @@ public class SplashScreen extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
-                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    startActivity(new Intent(SplashScreen.this, WelcomeActivity.class));
                 }
             }
         };
